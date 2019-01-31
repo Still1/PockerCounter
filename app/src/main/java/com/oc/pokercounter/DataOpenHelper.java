fa7b603353c350d1f315ebf735e5ed5036ee6466
Still1 <oliverstill1@gmail.com>
2019-02-01 00:46:56 +0800

    developing.
    Signed-off-by: Still1 <oliverstill1@gmail.com>

--- app/src/main/java/com/oc/pokercounter/DataOpenHelper.java
+++ app/src/main/java/com/oc/pokercounter/DataOpenHelper.java
@@ -7,7 +7,7 @@ import android.widget.Toast;
 
 public class DataOpenHelper extends SQLiteOpenHelper {
 
-    private static final int DB_VERSION = 2;
+    private static final int DB_VERSION = 4;
     private static final String DB_NAME = "pokerCounter";
     public static final String TABLE_NAME_PLAYER = "t_player";
     public static final String TABLE_NAME_MATCH = "t_match";
@@ -23,11 +23,14 @@ public class DataOpenHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String createPlayerSql = "create table if not exists " + TABLE_NAME_PLAYER + " (id integer primary key autoincrement, name text)";
         String createMatchSql = "create table if not exists " + TABLE_NAME_MATCH + " (id integer primary key autoincrement, match_time timestamp default(datetime('now', 'localtime')))";
-        String createMatchPlayerSql = "create table if not exists " + TABLE_NAME_MATCH_PLAYER + " (match_id integer primary key autoincrement, name text)";
-        String createGameSql = "create table if not exists " + TABLE_NAME_GAME + " (id integer primary key autoincrement, name text)";
-        String createGamePlayerSql = "create table if not exists " + TABLE_NAME_GAME_PLAYER + " (id integer primary key autoincrement, name text)";
+        String createMatchPlayerSql = "create table if not exists " + TABLE_NAME_MATCH_PLAYER + " (match_id integer, player_id integer, order_num, integer, score integer, primary key(match_id, player_id))";
+        String createGameSql = "create table if not exists " + TABLE_NAME_GAME + " (id integer primary key autoincrement, order_num integer, match_id integer, bomb integer, landlord_win integer)";
+        String createGamePlayerSql = "create table if not exists " + TABLE_NAME_GAME_PLAYER + " (game_id integer, player_id integer, landlord integer, score integer, primary key(game_id, player_id))";
         db.execSQL(createPlayerSql);
         db.execSQL(createMatchSql);
+        db.execSQL(createMatchPlayerSql);
+        db.execSQL(createGameSql);
+        db.execSQL(createGamePlayerSql);
     }
 
     @Override
--- app/src/main/java/com/oc/pokercounter/MainActivity.java
+++ app/src/main/java/com/oc/pokercounter/MainActivity.java
@@ -5,23 +5,22 @@ import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
     private DataOpenHelper dataOpenHelper;
 
-    private List<Map<String, Object>> headGridViewData;
+    private List<Map<String, Object>> matchPlayerList;
+    private List<String> matchPlayerNameList;
+    private List<Map<String, Object>> scoreGridViewData;
+
+    private Integer matchId;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -75,6 +78,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.new_match:
                 this.newMatch();
                 return true;
+            case R.id.add_game:
+                this.addGame();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -107,19 +113,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void newMatch() {
-        final List<String> yourChoices = new ArrayList<>();
+        this.matchPlayerList = new ArrayList<>();
 
-        final List<String> playerList = new ArrayList<String>();
+        final List<Map<String, Object>> playerList = new ArrayList<>();
+        final List<String> playerNameList = new ArrayList<>();
         SQLiteDatabase db = this.dataOpenHelper.getReadableDatabase();
-        Cursor cursor = db.query(DataOpenHelper.TABLE_NAME_PLAYER, new String[] {"id", "name"}, null, null, null, null, null);
+        Cursor cursor = db.query(DataOpenHelper.TABLE_NAME_PLAYER, new String[] {"id", "name"}, null, null, null, null, "id");
         if (cursor.getCount() > 0) {
             while (cursor.moveToNext()) {
-                playerList.add(cursor.getString(1));
+                Map<String, Object> playerMap = new HashMap<>();
+                playerMap.put("id", cursor.getInt(0));
+                playerMap.put("name", cursor.getString(1));
+                playerList.add(playerMap);
+                playerNameList.add(cursor.getString(1));
             }
         }
 
-        String[] items =  playerList.toArray(new String[playerList.size()]);
-        yourChoices.clear();
+        String[] items =  playerNameList.toArray(new String[playerList.size()]);
+        matchPlayerList.clear();
         AlertDialog.Builder multiChoiceDialog = new AlertDialog.Builder(MainActivity.this);
         multiChoiceDialog.setTitle("Please choose players");
         multiChoiceDialog.setMultiChoiceItems(items, null,
@@ -127,45 +138,83 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which, boolean isChecked) {
                         if (isChecked) {
-                            yourChoices.add(playerList.get(which));
+                            matchPlayerList.add(playerList.get(which));
+                            matchPlayerNameList.add((String)playerList.get(which).get("name"));
                         } else {
-                            yourChoices.remove(playerList.get(which));
+                            matchPlayerList.remove(playerList.get(which));
+                            matchPlayerNameList.remove(playerList.get(which).get("name"));
                         }
                     }
                 });
         multiChoiceDialog.setPositiveButton("confirm",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        SQLiteDatabase db = MainActivity.this.dataOpenHelper.getWritableDatabase();
-                        db.beginTransaction();
-                        db.execSQL("insert into " + DataOpenHelper.TABLE_NAME_MATCH + " (id, match_time) values (null, null)");
-                        db.setTransactionSuccessful();
-                        db.endTransaction();
-                        db.close();
-                        MainActivity.this.headGridViewData = new ArrayList<>();
-                        for(int i = 0; i < 1; i++) {
-                            for(String s : yourChoices) {
-                                Map<String, Object> map = new HashMap<String, Object>();
-                                map.put("text", s);
-                                MainActivity.this.headGridViewData.add(map);
-                            }
-                        }
-                        MainActivity.this.showHeadGridView();
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    SQLiteDatabase db = dataOpenHelper.getWritableDatabase();
+                    db.beginTransaction();
+                    db.execSQL("insert into " + DataOpenHelper.TABLE_NAME_MATCH + " (id, match_time) values (null, null)");
+                    Cursor cursor = db.rawQuery("select last_insert_rowid() from " + DataOpenHelper.TABLE_NAME_MATCH, null);
+                    cursor.moveToFirst();
+                    matchId = cursor.getInt(0);
 
+                    for(int i = 0; i < matchPlayerList.size(); i++) {
+                        ContentValues contentValues = new ContentValues();
+                        contentValues.put("match_id", matchId);
+                        contentValues.put("player_id", matchPlayerList.get(i).get("id").toString());
+                        contentValues.put("score", 0);
+                        contentValues.put("order_num", i);
+                        db.insert(DataOpenHelper.TABLE_NAME_MATCH_PLAYER, null, contentValues);
                     }
-                });
+                    db.setTransactionSuccessful();
+                    db.endTransaction();
+                    db.close();
+                    MainActivity.this.showHeadGridView();
+
+                }
+            });
+        multiChoiceDialog.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
         multiChoiceDialog.show();
     }
 
     private void showHeadGridView() {
-        String[] from = {"text"};
+        String[] from = {"name"};
         int[] to = {R.id.headGridViewText};
-        SimpleAdapter adapter = new SimpleAdapter(this, this.headGridViewData, R.layout.gridview_item, from, to);
+        SimpleAdapter adapter = new SimpleAdapter(this, this.matchPlayerList, R.layout.gridview_item, from, to);
         GridView headGridView = findViewById(R.id.headGridView);
         headGridView.setAdapter(adapter);
+    }
 
-        GridView scoreGridView = findViewById(R.id.scoreGridView);
-        scoreGridView.setAdapter(adapter);
+    private void addGame() {
+        final List<Map<String, Object>> landlordList = new ArrayList<>();
+        AlertDialog.Builder customizeDialog = new AlertDialog.Builder(MainActivity.this);
+        final View dialogView = LayoutInflater.from(MainActivity.this).inflate(R.layout.add_game,null);
+        customizeDialog.setTitle("Add a game");
+        customizeDialog.setView(dialogView);
+
+        String[] items =  this.matchPlayerNameList.toArray(new String[this.matchPlayerNameList.size()]);
+        customizeDialog.setMultiChoiceItems(items, null,
+            new DialogInterface.OnMultiChoiceClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                    if (isChecked) {
+                        landlordList.add(matchPlayerList.get(which));
+                    } else {
+                        landlordList.remove(matchPlayerList.get(which));
+                    }
+                }
+            });
+
+        customizeDialog.setPositiveButton("confirm",
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+        customizeDialog.show();
     }
 }
